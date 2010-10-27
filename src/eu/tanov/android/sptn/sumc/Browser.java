52f3b1fefefcd1f70d713d05ca55454304ddab82
Plamen Tanov <1.plamen@gmail.com>
2010-10-27 23:09:27 +0300

    Changed format of request to m.sumc.bg (they changed the site).

--- src/eu/tanov/android/sptn/sumc/Browser.java
+++ src/eu/tanov/android/sptn/sumc/Browser.java
@@ -1,16 +1,10 @@
 package eu.tanov.android.sptn.sumc;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Collections;
-import java.util.List;
-
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.CoreProtocolPNames;
 
 import android.util.Log;
@@ -18,9 +12,8 @@ import android.util.Log;
 public class Browser {
 	private static final String TAG = "SimpleBrowser";
 
-	private static final String PARAMETER_STATION_CODE = "stopCode";
 	private static final String USER_AGENT = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.8 (KHTML, like Gecko) Chrome/5.0.396.0 Safari/533.8";
-	private static final String URL = "http://m.sumc.bg/vt";
+	private static final String FORMAT_URL = "http://m.sumc.bg/vt?q=%s&go=1";
 
 	private ResponseHandler<String> responseHandler;
 	
@@ -31,7 +24,7 @@ public class Browser {
 		//XXX do not create client every time, use HTTP1.1 keep-alive!
         final HttpClient client = new DefaultHttpClient();
 
-        final HttpPost request = createRequest(stationCode);
+        final HttpGet request = createRequest(stationCode);
         
         // Create a response handler
         if (responseHandler == null) {
@@ -50,17 +43,10 @@ public class Browser {
         return result;
 	}
 
-	private static HttpPost createRequest(String stationCode) {
-		final HttpPost result = new HttpPost(URL);
+	private static HttpGet createRequest(String stationCode) {
+		final HttpGet result = new HttpGet(String.format(FORMAT_URL, stationCode));
         result.getParams().setParameter(CoreProtocolPNames.USER_AGENT, USER_AGENT);
         result.getParams().setBooleanParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, false);
-        
-        try {
-        	final List<BasicNameValuePair> parameters = Collections.singletonList(new BasicNameValuePair(PARAMETER_STATION_CODE, stationCode));
-			result.setEntity(new UrlEncodedFormEntity(parameters));
-		} catch (UnsupportedEncodingException e) {
-			throw new IllegalStateException("Default encoding is unsupported", e);
-		}
 		return result;
 	}
 }

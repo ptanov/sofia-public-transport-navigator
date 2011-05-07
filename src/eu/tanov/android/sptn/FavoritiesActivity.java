e8b6bc48cbb1aeafe4f0b3c3529c4c0c25bf5f3b
Plamen Tanov <1.plamen@gmail.com>
2011-05-07 15:12:53 +0300

    (#28) Added favorities service

--- src/eu/tanov/android/sptn/FavoritiesActivity.java
+++ src/eu/tanov/android/sptn/FavoritiesActivity.java
@@ -1,5 +1,8 @@
 package eu.tanov.android.sptn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -11,20 +14,35 @@ import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import eu.tanov.android.sptn.favorities.BusStopItem;
+import eu.tanov.android.sptn.favorities.FavoritiesService;
 
 public class FavoritiesActivity extends ListActivity {
-	final BusStopItem[] listItems = { new BusStopItem(1, 11, "aa"),
-			new BusStopItem(2, 22, "bb"), };
+	private final List<BusStopItem> busStops = new ArrayList<BusStopItem>();
+	private ArrayAdapter<BusStopItem> arrayAdapter;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.favorities);
-		setListAdapter(new ArrayAdapter<BusStopItem>(this,
-				android.R.layout.simple_list_item_1, listItems));
+		arrayAdapter = new ArrayAdapter<BusStopItem>(this, android.R.layout.simple_list_item_1, busStops);
+		refreshContent();
+		setListAdapter(arrayAdapter);
 		registerForContextMenu(findViewById(android.R.id.list));
 	}
-	
+
+	private FavoritiesService getFavoritiesService() {
+		// XXX how to pass this service across whole application?
+		return new FavoritiesService(this);
+	}
+	private void refreshContent() {
+		final FavoritiesService favoritiesService = getFavoritiesService();
+		busStops.clear();
+		busStops.addAll(favoritiesService.getAll());
+		
+		arrayAdapter.notifyDataSetChanged();
+	}
+
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
@@ -37,10 +55,19 @@ public class FavoritiesActivity extends ListActivity {
 		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
 		switch (item.getItemId()) {
 		case R.id.menu_favorities_up:
+			getFavoritiesService().move(arrayAdapter.getItem(info.position), -1);
+			
+			refreshContent();
 			return true;
 		case R.id.menu_favorities_down:
+			getFavoritiesService().move(arrayAdapter.getItem(info.position), +1);
+
+			refreshContent();
 			return true;
 		case R.id.menu_favorities_remove:
+			getFavoritiesService().remove(arrayAdapter.getItem(info.position));
+
+			refreshContent();
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
@@ -51,13 +78,13 @@ public class FavoritiesActivity extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);
 		final BusStopItem selectedBusStop = (BusStopItem) getListView().getItemAtPosition(position);
-		//TODO how to pass position? maybe using intent?
-//		getIntent().putExtra("code", selectedBusStop.getCode());
-		//from http://stackoverflow.com/questions/2497205/how-to-return-a-result-startactivityforresult-from-a-tabhost-activity:
+		// TODO how to pass position? maybe using intent?
+		// getIntent().putExtra("code", selectedBusStop.getCode());
+		// from http://stackoverflow.com/questions/2497205/how-to-return-a-result-startactivityforresult-from-a-tabhost-activity:
 		if (getParent() == null) {
-			setResult(RESULT_FIRST_USER+selectedBusStop.getCode());
+			setResult(RESULT_FIRST_USER + selectedBusStop.getCode());
 		} else {
-		    getParent().setResult(RESULT_FIRST_USER+selectedBusStop.getCode());
+			getParent().setResult(RESULT_FIRST_USER + selectedBusStop.getCode());
 		}
 		finish();
 	}
---
+++ src/eu/tanov/android/sptn/favorities/FavoritiesService.java
@@ -0,0 +1,155 @@
+package eu.tanov.android.sptn.favorities;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class FavoritiesService {
+
+	private static final String SHARED_PREFERENCES_NAME_FAVORITIES_POSITIONS_TO_CODE = "favorities_positions";
+	private static final String SHARED_PREFERENCES_NAME_FAVORITIES_CODE_TO_LABELS = "favorities_labels";
+	private final Context context;
+
+	private static Comparator<BusStopItem> comparator = new Comparator<BusStopItem>() {
+		@Override
+		public int compare(BusStopItem object1, BusStopItem object2) {
+			return object1.getPosition() - object2.getPosition();
+		}
+	};
+
+	public FavoritiesService(Context context) {
+		this.context = context;
+	}
+
+	private SharedPreferences getPositionsStore() {
+		return context.getSharedPreferences(SHARED_PREFERENCES_NAME_FAVORITIES_POSITIONS_TO_CODE, Context.MODE_PRIVATE);
+	}
+
+	private SharedPreferences getLabelsStore() {
+		return context.getSharedPreferences(SHARED_PREFERENCES_NAME_FAVORITIES_CODE_TO_LABELS, Context.MODE_PRIVATE);
+	}
+
+	public List<BusStopItem> getAll() {
+		final Map<String, ?> allPositions = getPositionsStore().getAll();
+		final Map<String, ?> allLabels = getLabelsStore().getAll();
+
+		final List<BusStopItem> result = new ArrayList<BusStopItem>(allPositions.size());
+
+		for (Entry<String, ?> entry : allPositions.entrySet()) {
+			result.add(busStopFromEntry(entry, allLabels));
+		}
+
+		// not the fastest way, but easiest:
+		Collections.sort(result, comparator);
+		return result;
+	}
+
+	private BusStopItem busStopFromEntry(Entry<String, ?> positionEntry, Map<String, ?> allLabels) {
+		final String position = positionEntry.getKey();
+		final Object code = positionEntry.getValue();
+		if (!(code instanceof Integer)) {
+			throw new IllegalStateException("Bus stop code should be integer, not: " + code);
+		}
+
+		final Object label = allLabels.get(code);
+		if (!(label instanceof String)) {
+			throw new IllegalStateException("Label for favorite bus stop <" + code + "> should be String, but was: " + label);
+		}
+		return new BusStopItem(Integer.valueOf(position), (Integer) code, (String) label);
+	}
+
+	/**
+	 * Move to end then remove
+	 */
+	public void remove(BusStopItem busStop) {
+		final SharedPreferences positionsStore = getPositionsStore();
+		final SharedPreferences.Editor positionsStoreEditor = positionsStore.edit();
+
+		final Map<String, ?> allPositions = positionsStore.getAll();
+		final int oldPosition = busStop.getPosition();
+		final int lastPosition = allPositions.size() - 1;
+		//move to end:
+		if (lastPosition != oldPosition) {
+			reorder(positionsStoreEditor, allPositions, oldPosition, lastPosition);
+		}
+		//remove last
+		positionsStoreEditor.remove(Integer.toString(lastPosition));
+		
+		positionsStoreEditor.commit();
+	}
+
+	public void add(BusStopItem busStop) {
+		final SharedPreferences positionsStore = getPositionsStore();
+		final SharedPreferences.Editor positionsStoreEditor = positionsStore.edit();
+		final SharedPreferences.Editor labelsStoreEditor = getLabelsStore().edit();
+
+		final int size = positionsStore.getAll().size();
+		positionsStoreEditor.putInt(Integer.toString(size), busStop.getCode());
+		labelsStoreEditor.putString(Integer.toString(busStop.getCode()), busStop.getLabel());
+
+		if (positionsStoreEditor.commit()) {
+			labelsStoreEditor.commit();
+		}
+	}
+
+	/**
+	 * If positionOffset is outside [0; size) - uses first valid position in boundary
+	 */
+	public void move(BusStopItem busStop, int positionOffset) {
+		final SharedPreferences positionsStore = getPositionsStore();
+		final SharedPreferences.Editor positionsStoreEditor = positionsStore.edit();
+
+		final int oldPosition = busStop.getPosition();
+		int newPosition = oldPosition + positionOffset;
+		if (newPosition < 0) {
+			newPosition = 0;
+		}
+		final Map<String, ?> allPositions = positionsStore.getAll();
+		final int lastPosition = allPositions.size() - 1;
+		if (newPosition > lastPosition) {
+			newPosition = lastPosition;
+		}
+
+		if (newPosition == oldPosition) {
+			return;
+		}
+
+		reorder(positionsStoreEditor, allPositions, oldPosition, newPosition);
+		
+		positionsStoreEditor.commit();
+	}
+
+	/**
+	 * Without checking for new position boundaries
+	 * 
+	 * @param originalPositions
+	 *            for optimization
+	 * @param newPosition
+	 *            should be in boundaries, not equal to old position!
+	 */
+	private void reorder(SharedPreferences.Editor positionsStoreEditor, Map<String, ?> originalPositions, int oldPosition, int newPosition) {
+		final Object busStopToMove = originalPositions.get(Integer.toString(oldPosition));
+		if (!(busStopToMove instanceof Integer)) {
+			throw new IllegalStateException("No usable information for position(" + oldPosition + "): " + busStopToMove);
+		}
+		
+		final int increment = oldPosition < newPosition ? 1 : -1;
+
+		for (int currentPosition = oldPosition; currentPosition != newPosition; currentPosition += increment) {
+			// swap currentPosition with next position
+			final int nextPosition = currentPosition + increment;
+			final Object object = originalPositions.get(Integer.toString(nextPosition));
+			if (!(object instanceof Integer)) {
+				throw new IllegalStateException("No usable information for position(" + nextPosition + "): " + object);
+			}
+			positionsStoreEditor.putInt(Integer.toString(currentPosition), (Integer) object);
+		}
+		positionsStoreEditor.putInt(Integer.toString(newPosition), (Integer)busStopToMove);
+	}
+}

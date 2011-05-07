aebb1f3193523d5aa8d20474071cb23da7195a0b
Plamen Tanov <1.plamen@gmail.com>
2011-05-07 13:43:40 +0300

    (#25) Added context menu with no actions

--- src/eu/tanov/android/sptn/FavoritiesActivity.java
+++ src/eu/tanov/android/sptn/FavoritiesActivity.java
@@ -2,7 +2,12 @@ package eu.tanov.android.sptn;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import eu.tanov.android.sptn.favorities.BusStopItem;
@@ -17,8 +22,31 @@ public class FavoritiesActivity extends ListActivity {
 		setContentView(R.layout.favorities);
 		setListAdapter(new ArrayAdapter<BusStopItem>(this,
 				android.R.layout.simple_list_item_1, listItems));
+		registerForContextMenu(findViewById(android.R.id.list));
 	}
 	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+		super.onCreateContextMenu(menu, v, menuInfo);
+		final MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.layout.favorities_menu, menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		final AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+		switch (item.getItemId()) {
+		case R.id.menu_favorities_up:
+			return true;
+		case R.id.menu_favorities_down:
+			return true;
+		case R.id.menu_favorities_remove:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		super.onListItemClick(l, v, position, id);

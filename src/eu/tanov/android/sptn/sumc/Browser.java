caa261410311059b7fa8ba78354c1df7ccd07a5f
Plamen Tanov <1.plamen@gmail.com>
2012-02-02 21:47:38 +0200

    (#68): Two requests are created - one for query parameters, second for estimates http://code.google.com/p/sofia-public-transport-navigator/issues/detail?id=68

--- src/eu/tanov/android/sptn/sumc/Browser.java
+++ src/eu/tanov/android/sptn/sumc/Browser.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
@@ -15,6 +16,10 @@ import org.apache.http.params.CoreProtocolPNames;
 import android.util.Log;
 
 public class Browser {
+
+    private static final String START_QUERY_BUS_STOP_ID = "<input type=\"text\"  value=\"\"  name=\"";
+    private static final String START_QUERY_O = "<input type=\"hidden\" name=\"";
+    private static final String START_QUERY_GO = "<input type=\"hidden\" value=\"1\" name=\"";
 
     private static final String TAG = "SimpleBrowser";
 
@@ -36,15 +41,12 @@ public class Browser {
         // XXX do not create client every time, use HTTP1.1 keep-alive!
         final HttpClient client = new DefaultHttpClient();
 
-        final HttpPost request = createRequest(stationCode);
-        if (request == null) {
-            return null;
-        }
-
         // Create a response handler
         if (responseHandler == null) {
             responseHandler = new BasicResponseHandler();
         }
+        final HttpPost request = createRequest(client, responseHandler, stationCode);
+
         String result = null;
         try {
             result = client.execute(request, responseHandler);
@@ -58,14 +60,47 @@ public class Browser {
         return result;
     }
 
-    private static HttpPost createRequest(String stationCode) {
+    private static HttpPost createRequest(HttpClient client, ResponseHandler<String> responseHandler, String stationCode) {
+        final HttpGet parametersRequest = createParametersRequest(stationCode);
+        try {
+            final String parametersResult = client.execute(parametersRequest, responseHandler);
+            final int busStopIdStart = parametersResult.indexOf(START_QUERY_BUS_STOP_ID);
+            final String queryBusStopId = getAttributeValue(parametersResult,
+                    busStopIdStart + START_QUERY_BUS_STOP_ID.length());
+            final int oStart = parametersResult.indexOf(START_QUERY_O,
+                    busStopIdStart + START_QUERY_BUS_STOP_ID.length());
+            final String queryO = getAttributeValue(parametersResult, oStart + START_QUERY_O.length());
+            final String queryGo = getAttributeValue(parametersResult,
+                    parametersResult.indexOf(START_QUERY_GO, oStart + START_QUERY_O.length()) + START_QUERY_GO.length());
+            return createRequest(stationCode, queryBusStopId, queryO, queryGo);
+        } catch (Exception e) {
+            Log.e(TAG, "Could not get data for parameters for station: " + stationCode, e);
+            return null;
+        }
+    }
+
+    private static String getAttributeValue(String parametersResult, int busStopIdStart) {
+        return parametersResult.substring(busStopIdStart, parametersResult.indexOf("\"", busStopIdStart));
+    }
+
+    private static HttpGet createParametersRequest(String stationCode) {
+        final HttpGet result = new HttpGet(URL);
+        result.addHeader("User-Agent", USER_AGENT);
+        result.addHeader("Referer", REFERER);
+        result.getParams().setBooleanParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, true);
+
+        return result;
+    }
+
+    private static HttpPost createRequest(String stationCode, String queryBusStopId, String queryO, String queryGo) {
         final HttpPost result = new HttpPost(URL);
         result.addHeader("User-Agent", USER_AGENT);
         result.addHeader("Referer", REFERER);
         result.getParams().setBooleanParameter(CoreProtocolPNames.USE_EXPECT_CONTINUE, false);
         try {
-            final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(Arrays.asList(new BasicNameValuePair("q",
-                    "000000" + stationCode), new BasicNameValuePair("o", "1"), new BasicNameValuePair("go", "1")));
+            final UrlEncodedFormEntity entity = new UrlEncodedFormEntity(Arrays.asList(new BasicNameValuePair(
+                    queryBusStopId,
+                    "000000" + stationCode), new BasicNameValuePair(queryO, "1"), new BasicNameValuePair(queryGo, "1")));
             result.setEntity(entity);
         } catch (UnsupportedEncodingException e) {
             throw new IllegalStateException("Not supported default encoding?", e);
